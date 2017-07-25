ff0d4ea8018c405268ba75967dd9846dab4814c7
tkir <kirill.titenko@gmail.com>
2017-07-25 23:35:04 +0300

    bugs resize fixed

--- src/app/editor/hr-editor/hr-editor.component.ts
+++ src/app/editor/hr-editor/hr-editor.component.ts
@@ -1,22 +1,40 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Line} from "../../data/Line";
+import {Store} from "../../data/store";
+import {DataService} from "../../data/data.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'card-hr-editor',
   templateUrl: './hr-editor.component.html',
   styleUrls: ['./hr-editor.component.css']
 })
-export class HrEditorComponent implements OnInit {
+export class HrEditorComponent implements OnInit, OnDestroy {
 
   @Input() hr: Line = null;
+
+  private subscription: Subscription = null;
+  background: any;
 
   //TODO to config
   allowedDesigns: string[] = ['solid', 'dashed', 'dotted', 'double'];
 
-  constructor() {
+  constructor(private dataService: DataService,
+              private store: Store) {
   }
 
   ngOnInit() {
+    this.dataService.setCardData();
+    this.subscription = this.store.changes
+      .subscribe((cardData: any) =>
+        this.background = cardData.background
+      );
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription)
+      this.subscription.unsubscribe();
+    this.subscription = null;
   }
 
   updateHr(param: string, res: any) {
@@ -26,6 +44,11 @@ export class HrEditorComponent implements OnInit {
       this.hr.thickness = res;
     else if (param == 'color')
       this.hr._color = res;
+  }
+
+  changeOrientation() {
+    this.hr.isHorizontal = !this.hr.isHorizontal;
+    this.hr.onChangeBgSize(this.background);
   }
 
 }
